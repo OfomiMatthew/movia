c35225d52994fe59efa4c2a8225e6b4dd0549eec
MatthewOfomi <ofomimatthew7@gmail.com>
2025-08-28 21:41:09 +0100

    deployment configuration done

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!uwehg41(molo=lmrs6$q#(ko!9rl!$5p=ul=db*&nqsm_bw*5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
